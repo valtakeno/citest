66e4822f99e32e6f24321d522a53287fdbbef53f
val00226 <takeno@val.co.jp>
2016-07-21 16:29:45 +0900

    after process test 5

--- .travis/after_success.sh
+++ .travis/after_success.sh
@@ -7,15 +7,15 @@ CHANNEL="#botchannel"
 # user name for direct message
 USER="@takeno"
 # slack user name
-BOTNAME="Jeffry"
+BOTNAME="Travis"
 # slack icon
-FACEICON="http://drive.google.com/uc?export=view&id=0B-6uJweum9gRR09Bd2V4bWpLdnc"
+#FACEICON="http://drive.google.com/uc?export=view&id=0B-6uJweum9gRR09Bd2V4bWpLdnc"
 
 MESSAGE=""
 
   MESSAGE=`echo -e $@`
 
-/bin/curl -s -S -X POST --data-urlencode \
+/usr/bin/curl -s -S -X POST --data-urlencode \
  "payload={ 
   \"channel\": \"${CHANNEL}\",
   \"username\": \"${BOTNAME}\",
