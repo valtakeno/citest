84141797df38a10b817f058ccc19d360ee4af63c
val00226 <takeno@val.co.jp>
2016-07-21 15:52:08 +0900

    after process test

---
+++ .travis/after_success.sh
@@ -0,0 +1,30 @@
+#!/bin/sh -xe
+
+# Incoming WebHooks URL
+WEBHOOKURL="https://hooks.slack.com/services/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"
+# slack channel
+CHANNEL="#botchannel"
+# user name for direct message
+USER="@takeno"
+# slack user name
+BOTNAME="Jeffry"
+# slack icon
+FACEICON="http://drive.google.com/uc?export=view&id=0B-6uJweum9gRR09Bd2V4bWpLdnc"
+
+MESSAGE=""
+
+  MESSAGE=`echo -e $@`
+
+/bin/curl -s -S -X POST --data-urlencode \
+ "payload={ 
+  \"channel\": \"${CHANNEL}\",
+  \"username\": \"${BOTNAME}\",
+  \"icon_url\": \"${FACEICON}\",
+  \"attachments\": [
+      {
+        \"color\": \"good\",
+        \"text\": \"${MESSAGE}\"
+      }
+    ]
+ }" \
+  ${WEBHOOKURL} >/dev/null
